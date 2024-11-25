2bca22385930c964b99ce34a7e847d23253b59a3
hwlei888 <leihuawen888@gmail.com>
2024-11-26 02:01:53 +1100

    add Backlog & ProjectTask under domain & repositories

---
+++ ppmtool/src/main/java/com/javaPpmTool/ppmtool/repositories/ProjectTaskRepository.java
@@ -0,0 +1,9 @@
+package com.javaPpmTool.ppmtool.repositories;
+
+import com.javaPpmTool.ppmtool.domain.ProjectTask;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProjectTaskRepository extends CrudRepository<ProjectTask, Long> {
+}
