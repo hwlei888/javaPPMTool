48724304351a603492ec5572303abfe44750ced4
hwlei888 <leihuawen888@gmail.com>
2024-11-28 00:22:52 +1100

    entity relationships between project and backlog

--- ppmtool/src/main/java/com/javaPpmTool/ppmtool/domain/Backlog.java
+++ ppmtool/src/main/java/com/javaPpmTool/ppmtool/domain/Backlog.java
@@ -1,9 +1,8 @@
 package com.javaPpmTool.ppmtool.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import jakarta.persistence.*;
+import org.springframework.validation.FieldError;
 
 @Entity
 public class Backlog {
@@ -15,6 +14,12 @@ public class Backlog {
     private String projectIdentifier;
 
     //OneToOne with project
+    //'project' need to match with mappedBy = "project" under Project.java
+    @OneToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "project_id", nullable = false)
+    @JsonIgnore
+    //jsonignore stop the recursion when no id when create project
+    private Project project;
     //OneToMany project tasks
 
     public Backlog() {
@@ -42,5 +47,13 @@ public class Backlog {
 
     public void setProjectIdentifier(String projectIdentifier) {
         this.projectIdentifier = projectIdentifier;
+    }
+
+    public Project getProject() {
+        return project;
+    }
+
+    public void setProject(Project project) {
+        this.project = project;
     }
 }
--- ppmtool/src/main/java/com/javaPpmTool/ppmtool/services/ProjectService.java
+++ ppmtool/src/main/java/com/javaPpmTool/ppmtool/services/ProjectService.java
@@ -1,7 +1,9 @@
 package com.javaPpmTool.ppmtool.services;
 
+import com.javaPpmTool.ppmtool.domain.Backlog;
 import com.javaPpmTool.ppmtool.domain.Project;
 import com.javaPpmTool.ppmtool.exceptions.ProjectIdException;
+import com.javaPpmTool.ppmtool.repositories.BacklogRepository;
 import com.javaPpmTool.ppmtool.repositories.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -12,9 +14,26 @@ public class ProjectService {
     @Autowired
     private ProjectRepository projectRepository;
 
+    @Autowired
+    private BacklogRepository backlogRepository;
+
     public Project saveOrUpdateProject(Project project){
         try{
             project.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+
+            //create a new project
+            if(project.getId() == null){
+                Backlog backlog = new Backlog();
+                project.setBacklog(backlog);
+                backlog.setProject(project);
+                backlog.setProjectIdentifier(project.getProjectIdentifier().toUpperCase());
+            }
+
+            //update the project
+            if(project.getId() != null){
+                project.setBacklog(backlogRepository.findByProjectIdentifier(project.getProjectIdentifier().toUpperCase()));
+            }
+
             return projectRepository.save(project);
         }catch (Exception e){
             throw new ProjectIdException("Project ID '" + project.getProjectIdentifier().toUpperCase() + "' already exists");
