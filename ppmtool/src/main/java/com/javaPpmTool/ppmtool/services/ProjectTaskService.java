509ddf4553b230c0f6f4604e9db057a51b47accd
hwlei888 <leihuawen888@gmail.com>
2024-12-03 01:29:38 +1100

    build ProjectTaskService & BacklogController

--- ppmtool/src/main/java/com/javaPpmTool/ppmtool/services/ProjectTaskService.java
+++ ppmtool/src/main/java/com/javaPpmTool/ppmtool/services/ProjectTaskService.java
@@ -1,5 +1,6 @@
 package com.javaPpmTool.ppmtool.services;
 
+import com.javaPpmTool.ppmtool.domain.Backlog;
 import com.javaPpmTool.ppmtool.domain.ProjectTask;
 import com.javaPpmTool.ppmtool.repositories.BacklogRepository;
 import com.javaPpmTool.ppmtool.repositories.ProjectTaskRepository;
@@ -15,15 +16,38 @@ public class ProjectTaskService {
     @Autowired
     private ProjectTaskRepository projectTaskRepository;
 
-    public ProjectTask addProjectTask(){
+    public ProjectTask addProjectTask(String projectIdentifier, ProjectTask projectTask){
+
+        //Exceptions: Project not found
+
         //ProjectTasks to be added to a specific project, project != null, BackLog exists
+        Backlog backlog = backlogRepository.findByProjectIdentifier(projectIdentifier);
+
         //set the relationship backlog to projectTask
-        //we want our project sequence to be like this: IDPRO-1 IDPRO-2 ... 100 101
+        projectTask.setBacklog(backlog);
+
+        //we want our project sequence to be like this: IDPRO-1 IDPRO-2 ... IDPRO-100 IDPRO-101
+        Integer BacklogSequence = backlog.getPTSequence();
         //Update the BackLog SEQUENCE
+        BacklogSequence++;
+        backlog.setPTSequence(BacklogSequence);
+
+        //Add sequence to Project Task
+        //projectTask.setProjectIdentifier(backlog.getProjectIdentifier()+"-"+BacklogSequence);
+        projectTask.setProjectSequence(projectIdentifier+"-"+BacklogSequence);
+        projectTask.setProjectIdentifier(projectIdentifier);
 
         //INITIAL priority when priority null
+        //In the future we need if(projectTask.getPriority() == 0 || projectTask.getPriority() == null) to handle the form
+        if(projectTask.getPriority() == null){
+            projectTask.setPriority(3);
+        }
+
         //INITIAL status when status is null
+        if(projectTask.getStatus() == "" || projectTask.getStatus() == null){
+            projectTask.setStatus("TO_DO");
+        }
+
+        return projectTaskRepository.save(projectTask);
     }
-
-
 }
---
+++ ppmtool/src/main/java/com/javaPpmTool/ppmtool/web/BacklogController.java
@@ -0,0 +1,36 @@
+package com.javaPpmTool.ppmtool.web;
+
+import com.javaPpmTool.ppmtool.domain.ProjectTask;
+import com.javaPpmTool.ppmtool.services.MapValidationErrorService;
+import com.javaPpmTool.ppmtool.services.ProjectTaskService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/backlog")
+@CrossOrigin
+public class BacklogController {
+
+    @Autowired
+    private ProjectTaskService projectTaskService;
+
+    @Autowired
+    private MapValidationErrorService mapValidationErrorService;
+
+    @PostMapping("/{backlog_id}")
+    public ResponseEntity<?> addPTtoBacklog(@Valid @RequestBody ProjectTask projectTask, BindingResult result, @PathVariable String backlog_id){
+        ResponseEntity<?> errorMap = mapValidationErrorService.MapValidationService(result);
+        if(errorMap != null) return errorMap;
+
+        ProjectTask projectTask1 = projectTaskService.addProjectTask(backlog_id, projectTask);
+
+        return new ResponseEntity<ProjectTask>(projectTask1, HttpStatus.CREATED);
+    }
+
+
+
+}
