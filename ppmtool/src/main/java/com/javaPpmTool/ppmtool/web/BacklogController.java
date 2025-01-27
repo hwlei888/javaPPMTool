5174f90e57e3dbdf7abed2501338cfdbd1c6e84e
hwlei888 <leihuawen888@gmail.com>
2025-01-27 11:00:43 +1100

    Find ProjectTask by project Sequence

--- ppmtool/src/main/java/com/javaPpmTool/ppmtool/web/BacklogController.java
+++ ppmtool/src/main/java/com/javaPpmTool/ppmtool/web/BacklogController.java
@@ -38,7 +38,18 @@ public class BacklogController {
         return projectTaskService.findBacklogById(backlog_id);
     }
 
-//    public ResponseEntity<List<ProjectTask>> getProjectBacklog(@PathVariable String backlog_id){
-//        return new ResponseEntity<List<ProjectTask>>(projectTaskService.findBacklogById(backlog_id), HttpStatus.OK);
-//    }
+    /*
+    public ResponseEntity<List<ProjectTask>> getProjectBacklog(@PathVariable String backlog_id){
+        return new ResponseEntity<List<ProjectTask>>(projectTaskService.findBacklogById(backlog_id), HttpStatus.OK);
+    }
+    */
+
+    @GetMapping("/{backlog_id}/{pt_id}")
+    public ResponseEntity<?> getProjectTask(@PathVariable String backlog_id, @PathVariable String pt_id){
+        ProjectTask projectTask = projectTaskService.findPTByProjectSequence(backlog_id, pt_id);
+        return new ResponseEntity<ProjectTask>(projectTask, HttpStatus.OK);
+    }
+
+
+
 }
