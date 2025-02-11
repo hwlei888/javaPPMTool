3f420a44333559f041f0ed6c930ea4e30e3267c4
hwlei888 <leihuawen888@gmail.com>
2025-02-12 01:33:28 +1100

    add login part & update securityConfig

--- src/main/java/com/javaPpmTool/ppmtool/security/SecurityConfig.java
+++ src/main/java/com/javaPpmTool/ppmtool/security/SecurityConfig.java
@@ -1,13 +1,22 @@
 package com.javaPpmTool.ppmtool.security;
 
+import com.javaPpmTool.ppmtool.services.CustomUserDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+
+import static com.javaPpmTool.ppmtool.security.SecurityConstants.H2_URL;
+import static com.javaPpmTool.ppmtool.security.SecurityConstants.SIGN_UP_URLS;
 
 
 @Configuration
@@ -21,6 +30,26 @@ public class SecurityConfig {
 
     @Autowired
     private JwtAuthenticationEntryPoint unauthorizedHandler;
+
+    @Autowired
+    private CustomUserDetailsService customUserDetailsService;
+
+    @Autowired
+    private BCryptPasswordEncoder bCryptPasswordEncoder;
+
+
+    // Original outdated code
+//    @Override
+//    protected void configure(AuthenticationManagerBuilder authenticationManagerBuilder) throws Exception {
+//        authenticationManagerBuilder.userDetailsService(customUserDetailsService).passwordEncoder(bCryptPasswordEncoder);
+//    }
+//
+//    @Override
+//    @Bean(BeanIds.AUTHENTICATION_MANAGER)
+//    protected AuthenticationManager authenticationManager() throws Exception {
+//        return super.authenticationManager();
+//    }
+
 
 //    @Bean
 //    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
@@ -46,6 +75,22 @@ public class SecurityConfig {
 //                .anyRequest().authenticated();
 //    };
 
+
+    @Bean
+    public AuthenticationProvider authenticationProvider(){
+        DaoAuthenticationProvider daoAuthenticationProvider = new DaoAuthenticationProvider();
+        daoAuthenticationProvider.setUserDetailsService(customUserDetailsService);
+        daoAuthenticationProvider.setPasswordEncoder(bCryptPasswordEncoder);
+
+        return daoAuthenticationProvider;
+    }
+
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception{
         http
@@ -62,7 +107,8 @@ public class SecurityConfig {
                                 "/",
                                 "/favicon.ico"
                         ).permitAll()
-                        .requestMatchers("/api/users/**").permitAll()
+                        .requestMatchers(SIGN_UP_URLS).permitAll()
+                        .requestMatchers(H2_URL).permitAll()
                         .anyRequest().authenticated() //anything other than that we need to have authentication
                 );
 
